58f3134de1d715cbf6fd3b6bd7d7084b220ade87
mmichaelfelipe <michaelfelipe.ti@gmail.com>
2024-05-02 07:22:23 -0300

    🔨 feat: Implements ListarVeiculos method
    Signed-off-by: mmichaelfelipe <michaelfelipe.ti@gmail.com>

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -56,8 +56,10 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach (string veiculo in veiculos) 
+                {
+                    Console.WriteLine($"Placa: {veiculo}");
+                }
             }
             else
             {
